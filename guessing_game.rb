3855a76b0554c130c45ad8ddf8c220922565c651
James Webb <webbfamily@Jamess-MacBook-Pro.local>
2017-03-14 15:45:38 -0400

    Defined variables for chances

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,30 +1,39 @@
-quickpick = rand (1..100)
-puts quickpick
+def quickpick
+  rand (1..100)
+end
+
+def high(guess, random)
+  guess > random
+end
+
+def low(guess, random)
+  guess < random
+end
+
+def winner(guess, random)
+  guess == random
+end
+
+picks = []
+random_number = quickpick
+puts random_number
 count = 0
-puts "Guess any number between 1 and 100"
 
-def high
 
-end
-
-def low
-
-end
-
-def winner
-
-end
 
 loop do
   until count == 5
+    puts "Guess any number between 1 and 100"
     user_guess = gets.chomp.to_i
-    if user_guess < quickpick
+    if low(user_guess, random_number)
       puts "You're too low. Guess again."
       count += 1
-    elsif user_guess > quickpick
+      picks << user_guess
+    elsif high(user_guess, random_number)
       puts "You're too high. Guess again."
       count += 1
-    else user_guess = quickpick
+      picks << user_guess
+    else winner(user_guess, random_number)
       puts "Ching ching."
       break
     end
